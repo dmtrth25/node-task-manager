5ee1c6a99523a4fcafb3e598cdafaaf03131b4c3
dmtrth25 <vodolazhchenko25@gmail.com>
2022-03-28 12:44:16 +0300

    was made code refactoring, added error-handler

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const connectDB = require('./db/connect')
 const app = express()
 const tasks = require('./routes/tasks')
 require('dotenv').config()
-const notFound = require('./middleware/not-found')
+const notFound = require('./middleware/not-found');
+const handleError = require('./middleware/error-hadler')
 
 // middleware
 app.use(express.static('./public'))
 app.use(express.json())
-app.use(notFound)
-
-const PORT = process.env.PORT || 3030
 
 // routes
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(handleError)
 
-
+const PORT = process.env.PORT || 3030
 
 const start = async () => {
    try {
